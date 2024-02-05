34cbae9ec694a5e969d5359a21b4d865a0168c31
maskudo <manujkarki101@gmail.com>
2024-02-05 21:05:54 +0545

    feat: group ascending and descending

--- lua/bharyang/init.lua
+++ lua/bharyang/init.lua
@@ -11,11 +11,11 @@ M.BharyangDesc = function(_)
 	vim.api.nvim_buf_set_lines(0, line_start, line_end, true, lines)
 end
 
-M.BharyangGroup = function(_)
+local BharyangGroup = function(order_func)
 	local line_start, line_end, abs, rel, other = unpack(lib.group())
-	local _, _, abs_sorted = unpack(lib.asc(abs))
-	local _, _, rel_sorted = unpack(lib.asc(rel))
-	local _, _, other_sorted = unpack(lib.asc(other))
+	local _, _, abs_sorted = unpack(order_func(abs))
+	local _, _, rel_sorted = unpack(order_func(rel))
+	local _, _, other_sorted = unpack(order_func(other))
 	table.insert(abs_sorted, "\r")
 	for _, line in ipairs(rel_sorted) do
 		table.insert(abs_sorted, line)
@@ -27,10 +27,18 @@ M.BharyangGroup = function(_)
 	vim.api.nvim_buf_set_lines(0, line_start, line_end, true, abs_sorted)
 end
 
+M.BharyangGroupDesc = function()
+	return BharyangGroup(lib.desc)
+end
+M.BharyangGroupAsc = function()
+	return BharyangGroup(lib.asc)
+end
+
 M.setup = function(_)
 	vim.api.nvim_create_user_command("BharyangAsc", M.BharyangAsc, { range = true })
 	vim.api.nvim_create_user_command("BharyangDesc", M.BharyangDesc, { range = true })
-	vim.api.nvim_create_user_command("BharyangGroup", M.BharyangGroup, { range = true })
+	vim.api.nvim_create_user_command("BharyangGroupAsc", M.BharyangGroupAsc, { range = true })
+	vim.api.nvim_create_user_command("BharyangGroupDesc", M.BharyangGroupDesc, { range = true })
 end
 
 return M
