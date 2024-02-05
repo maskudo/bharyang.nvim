3a51c31cfca676588141ffce92d47bb5837f1bbd
maskudo <manujkarki101@gmail.com>
2024-02-05 21:02:00 +0545

    feat: group reorganize

--- lua/bharyang/init.lua
+++ lua/bharyang/init.lua
@@ -1,19 +1,36 @@
 local lib = require("bharyang.lib")
 local M = {}
 
-M.BharyangAsc = function(args)
-  local line_start, line_end, lines = unpack(lib.asc())
-  vim.api.nvim_buf_set_lines(0, line_start, line_end, true, lines)
+M.BharyangAsc = function(_)
+	local line_start, line_end, lines = unpack(lib.asc())
+	vim.api.nvim_buf_set_lines(0, line_start, line_end, true, lines)
 end
 
-M.BharyangDesc = function(args)
-  local line_start, line_end, lines = unpack(lib.desc())
-  vim.api.nvim_buf_set_lines(0, line_start, line_end, true, lines)
+M.BharyangDesc = function(_)
+	local line_start, line_end, lines = unpack(lib.desc())
+	vim.api.nvim_buf_set_lines(0, line_start, line_end, true, lines)
+end
+
+M.BharyangGroup = function(_)
+	local line_start, line_end, abs, rel, other = unpack(lib.group())
+	local _, _, abs_sorted = unpack(lib.asc(abs))
+	local _, _, rel_sorted = unpack(lib.asc(rel))
+	local _, _, other_sorted = unpack(lib.asc(other))
+	table.insert(abs_sorted, "\r")
+	for _, line in ipairs(rel_sorted) do
+		table.insert(abs_sorted, line)
+	end
+	table.insert(abs_sorted, "\r")
+	for _, line in ipairs(other_sorted) do
+		table.insert(abs_sorted, line)
+	end
+	vim.api.nvim_buf_set_lines(0, line_start, line_end, true, abs_sorted)
 end
 
 M.setup = function(_)
-  vim.api.nvim_create_user_command("BharyangAsc", M.BharyangAsc, { range = true })
-  vim.api.nvim_create_user_command("BharyangDesc", M.BharyangDesc, { range = true })
+	vim.api.nvim_create_user_command("BharyangAsc", M.BharyangAsc, { range = true })
+	vim.api.nvim_create_user_command("BharyangDesc", M.BharyangDesc, { range = true })
+	vim.api.nvim_create_user_command("BharyangGroup", M.BharyangGroup, { range = true })
 end
 
 return M
--- lua/bharyang/lib/init.lua
+++ lua/bharyang/lib/init.lua
@@ -1,42 +1,73 @@
+local abs_pattern = "^%s*import%s*.-%s*from%s*'[^%.].-';%s*$"
+local rel_pattern = "^%s*import%s*.-%s*from%s*'%.+.-';%s*$"
 local function get_visual_lines()
-  local vstart = vim.fn.getpos("'<")
+	local vstart = vim.fn.getpos("'<")
 
-  local vend = vim.fn.getpos("'>")
+	local vend = vim.fn.getpos("'>")
 
-  local line_start = vstart[2] - 1
-  local line_end = vend[2]
+	local line_start = vstart[2] - 1
+	local line_end = vend[2]
 
-  local lines = vim.api.nvim_buf_get_lines(0, line_start, line_end, false)
-  return { line_start, line_end, lines }
+	local lines = vim.api.nvim_buf_get_lines(0, line_start, line_end, false)
+	return { line_start, line_end, lines }
 end
 
 local function sort_desc(lines)
-  table.sort(lines, function(a, b)
-    return #a > #b
-  end)
-  return lines
+	table.sort(lines, function(a, b)
+		return #a > #b
+	end)
+	return lines
 end
 
 local function sort_asc(lines)
-  table.sort(lines, function(a, b)
-    return #a < #b
-  end)
-  return lines
+	table.sort(lines, function(a, b)
+		return #a < #b
+	end)
+	return lines
 end
 
-local function descending()
-  local line_start, line_end, lines = unpack(get_visual_lines())
-  local sorted = sort_desc(lines)
-  return { line_start, line_end, sorted }
+local function group_lines(lines)
+	local abs = {}
+	local rel = {}
+	local other = {}
+	for _, line in ipairs(lines) do
+		if line:match(abs_pattern) then
+			table.insert(abs, line)
+		elseif line:match(rel_pattern) then
+			table.insert(rel, line)
+		else
+			table.insert(other, line)
+		end
+	end
+	return abs, rel, other
 end
 
-local function ascending()
-  local line_start, line_end, lines = unpack(get_visual_lines())
-  local sorted = sort_asc(lines)
-  return { line_start, line_end, sorted }
+local function group()
+	local line_start, line_end, lines = unpack(get_visual_lines())
+	local abs, rel, other = group_lines(lines)
+	return { line_start, line_end, abs, rel, other }
+end
+
+local function descending(ls)
+	local line_start, line_end, lines = unpack(get_visual_lines())
+	if ls ~= nil then
+		lines = ls
+	end
+	local sorted = sort_desc(lines)
+	return { line_start, line_end, sorted }
+end
+
+local function ascending(ls)
+	local line_start, line_end, lines = unpack(get_visual_lines())
+	if ls ~= nil then
+		lines = ls
+	end
+	local sorted = sort_asc(lines)
+	return { line_start, line_end, sorted }
 end
 
 return {
-  asc = ascending,
-  desc = descending,
+	asc = ascending,
+	desc = descending,
+	group = group,
 }
