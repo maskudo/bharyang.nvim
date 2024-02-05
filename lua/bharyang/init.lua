9dd1f12e6124a8f5989b4f2e8321d911efe0182c
maskudo <manujkarki101@gmail.com>
2024-02-05 18:24:17 +0545

    feat: add user_command

--- lua/bharyang/init.lua
+++ lua/bharyang/init.lua
@@ -1,16 +1,19 @@
 local lib = require("bharyang.lib")
+local M = {}
 
-function BharyangAsc()
+M.BharyangAsc = function(args)
   local line_start, line_end, lines = unpack(lib.asc())
   vim.api.nvim_buf_set_lines(0, line_start, line_end, true, lines)
 end
 
-function BharyangDesc()
+M.BharyangDesc = function(args)
   local line_start, line_end, lines = unpack(lib.desc())
   vim.api.nvim_buf_set_lines(0, line_start, line_end, true, lines)
 end
 
-return {
-  BharyangAsc = BharyangAsc,
-  BharyangDesc = BharyangDesc,
-}
+M.setup = function(_)
+  vim.api.nvim_create_user_command("BharyangAsc", M.BharyangAsc, { range = true })
+  vim.api.nvim_create_user_command("BharyangDesc", M.BharyangDesc, { range = true })
+end
+
+return M
